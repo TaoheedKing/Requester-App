a5734141ddae76096f98200c308e768e084ff1e2
TaoheedKing <tking92@montgomerycollege.edu>
2021-08-04 00:43:06 -0400

    Partially implemented javascript functions

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,128 @@
 $(function() {
   var client = ZAFClient.init();
   client.invoke('resize', { width: '100%', height: '120px' });
+
+  // client.get('ticket.requester.id').then(
+  //   function(data) {
+  //     var user_id = data['ticket.requester.id'];
+  //     requestUserInfo(client, user_id);
+  //   }
+  // );
+
+  //Single ticket 
+  client.get('ticket.id').then(
+    function(data) {
+      var user_id = data['ticket.id'];
+      requestUserInfo(client, user_id);
+    }
+  );
+
 });
-$(function() {
-  var client = ZAFClient.init();
-  client.invoke('resize', { width: '100%', height: '120px' });
-  showInfo();
-});
+
+//SINGLE TICKET
+function requestTicketInfo(client, id) {
+  var settings = {
+    url: '/api/v2/search.json?query='+ id,
+    type:'GET',
+    dataType: 'json',
+  };
+
+  client.request(settings).then(
+    function(data) {
+      showInfo(data);
+    },
+    function(response) {
+      showError(response);
+    }
+  );
+}
+
+//ALL TICKET
+function requestAllTicket(client, index) {
+  var settings = {
+    url: '/api/v2/search/export?query=filter[type]=ticket&page[size]=100',
+    type:'GET',
+    dataType: 'json',
+  };
+
+  client.request(settings).then(
+    function(data) {
+      showInfo(data);
+    },
+    function(response) {
+      showError(response);
+    }
+  );
+}
+
+
+function requestUserInfo(client, id) {
+  var settings = {
+    url: '/api/v2/users/' + id + '.json',
+    type:'GET',
+    dataType: 'json',
+  };
+
+  client.request(settings).then(
+    function(data) {
+      showInfo(data);
+    },
+    function(response) {
+      showError(response);
+    }
+  );
+}
+
+
+//VIEW TICKET
+function showInfo(data) {
+  var requester_data = {
+    'name': data.user.name,
+    'tags': data.user.tags,
+    'created_at': formatDate(data.user.created_at),
+    'last_login_at': formatDate(data.user.last_login_at)
+  };
+
+  var source = $("#requester-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(requester_data);
+  $("#content").html(html);
+}
+
+// function showInfo(data) {
+//   var requester_data = {
+//     'name': data.user.name,
+//     'tags': data.user.tags,
+//     'created_at': formatDate(data.user.created_at),
+//     'last_login_at': formatDate(data.user.last_login_at)
+//   };
+
+//   var source = $("#requester-template").html();
+//   var template = Handlebars.compile(source);
+//   var html = template(requester_data);
+//   $("#content").html(html);
+// }
+
+
+function showError(response) {
+  var error_data = {
+    'status': response.status,
+    'statusText': response.statusText
+  };
+  var source = $("#error-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(error_data);
+  $("#content").html(html);
+}
+
+
+function formatDate(date) {
+  var cdate = new Date(date);
+  var options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  };
+  date = cdate.toLocaleDateString("en-us", options);
+  return date;
+}
